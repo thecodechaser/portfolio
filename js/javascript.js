961673bf4b64b93816a9afaa0d5dc780675c0d44
Ranjeet Singh <83803414+thecodechaser@users.noreply.github.com>
2021-11-09 00:23:27 +0530

    Update linter config files

--- js/javascript.js
+++ js/javascript.js
@@ -1,22 +1,19 @@
-let button = document.querySelector(".menu-button-btn");
+let button = document.querySelector(".btn-open");
+let button2 = document.querySelector(".btn-close");
 
 function display() {
   let menuContainer = document.querySelector(".mobile-menu-container");
   menuContainer.style.display = "block";
   let buttonContainer = document.querySelector(".btn-container-open");
-
   buttonContainer.style.display = "none";
 }
-
-let button2 = document.querySelector(".menu-button-btn-close");
 
 function display2() {
   let menuContainer = document.querySelector(".mobile-menu-container");
   menuContainer.style.display = "none";
   let buttonContainer = document.querySelector(".btn-container-open");
-
   buttonContainer.style.display = "block";
 }
 
 button.addEventListener("click", display);
-button2.addEventListener('click', display2);
+button2.addEventListener("click", display2);
