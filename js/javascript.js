a1aa4b02432996e5933ac57151ae373a5e1cc250
Ranjeet Singh <83803414+thecodechaser@users.noreply.github.com>
2021-11-09 01:39:35 +0530

    Fix linter errors

--- js/javascript.js
+++ js/javascript.js
@@ -19,6 +19,6 @@ function display2() {
 button.addEventListener('click', display);
 button2.addEventListener('click', display2);
 
-menuItems.forEach(function items(item) {
+menuItems.forEach(function (item) {
   item.addEventListener('click', display2);
 });
