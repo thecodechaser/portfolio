015d87e58e8325528c7e825e0b4c841749bdaafd
Natalia Poletaeva <91270103+NataliaPoletaeva@users.noreply.github.com>
2021-11-08 20:30:24 +0500

    Add close button for the menu

--- js/javascript.js
+++ js/javascript.js
@@ -8,4 +8,14 @@ function display() {
   buttonContainer.style.display = "none";
 }
 
+let button2 = document.querySelector(".menu-button-btn-close");
+
+function display2() {
+  let menuContainer = document.querySelector(".mobile-menu-container");
+  menuContainer.style.display = "none";
+  let buttonContainer = document.querySelector(".btn-container-open");
+
+  buttonContainer.style.display = "block";
+}
+
 button.addEventListener("click", display);
