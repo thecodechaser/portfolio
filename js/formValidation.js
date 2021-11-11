db599c2ab649e670b0eea573b7b23b3dd5be3360
Ranjeet Singh <83803414+thecodechaser@users.noreply.github.com>
2021-11-11 14:06:20 +0530

    Fix linter errors

--- js/formValidation.js
+++ js/formValidation.js
@@ -10,12 +10,12 @@ function validateEmail(input) {
   const value = input.toLowerCase();
   if (value.localeCompare(input) === 0) {
     return true;
-  } 
-   return false;
+  }
+  return false;
 }
 
-form.addEventListener("submit", (event) => {
-  const msg = document.querySelector(".validation-msg");
+form.addEventListener('submit', (event) => {
+  const msg = document.querySelector('.validation-msg');
   event.preventDefault();
   const input = form.elements[1];
   const emailValid = validateEmail(input.value);
