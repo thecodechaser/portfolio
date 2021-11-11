b7b42aed14e549b0ac01ed3804c01ce0ee3b4035
Ranjeet Singh <83803414+thecodechaser@users.noreply.github.com>
2021-11-11 13:57:31 +0530

    Fix linter errors

--- js/formValidation.js
+++ js/formValidation.js
@@ -1,5 +1,5 @@
 const form = document.querySelector('.form-element');
-const message = "Email must contain only lower case characters";
+const message = 'Email must contain only lower case characters';
 
 function showMessage(input, message) {
   const msg = document.querySelector('.validation-msg');
@@ -7,20 +7,19 @@ function showMessage(input, message) {
 }
 
 function validateEmail(input) {
-  const value = input.value;
-  const value1 = value.toLowerCase();
-  if (value.localeCompare(value1) == 0) {
+  const value = input.toLowerCase();
+  if (value.localeCompare(input) === 0) {
     return true;
   } else {
-    false;
+   return false;
   }
 }
 
 form.addEventListener('submit', function (event) {
   const msg = document.querySelector('.validation-msg');
   event.preventDefault();
-  let input = form.elements['email'];
-  const emailValid = validateEmail(input);
+  const input = form.elements['email'];
+  const emailValid = validateEmail(input.value);
   if (emailValid) {
     msg.remove();
     form.submit();
