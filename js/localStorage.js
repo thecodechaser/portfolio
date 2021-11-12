ab5a8afdebca3f10907b36006abb071ee6455f9a
koomekevo <koome.kevo@gurucrafts.net>
2021-11-12 11:16:55 +0300

    Add populateStorage() function

--- js/localStorage.js
+++ js/localStorage.js
@@ -2,10 +2,22 @@
  const email1 = document.querySelector('#email');
 const message1 = document.querySelector('#msg');
 
- const storeValues = (name1, email1, message1) => {
+populateStorage();
+ 
+const storeValues = (name1, email1, message1) => {
   const dataObject = JSON.stringify({ name1, email1, message1 });
   localStorage.setItem('dataObject', dataObject);
 };
+
+function populateStorage() {
+  const formValues = JSON.parse(localStorage.getItem('dataObject'));
+  if (formValues) {
+    name1.value = formValues.name1;
+    email1.value = formValues.email1;
+    message1.value = formValues.message1;
+  }
+}
+
 
  
 
